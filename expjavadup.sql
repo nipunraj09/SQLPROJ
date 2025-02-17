c745b32944c811b651c8d3cd0b864efb9ca428b4
Nipun raj <103949203+nipunraj09@users.noreply.github.com>
2025-02-17 13:11:44 +0530

    Create expjavadup.sql

---
+++ expjavadup.sql
@@ -0,0 +1,8 @@
+WITH CTE AS (
+    SELECT Id, ROLL, ROW_NUMBER() OVER (PARTITION BY ROLL ORDER BY Id) AS row_num
+    FROM expjava
+)
+DELETE FROM expjava
+WHERE Id IN (
+    SELECT Id FROM CTE WHERE row_num > 1
+);
