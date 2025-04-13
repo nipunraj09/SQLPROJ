39ffa4cfa977984bd86240d090b813c101643971
Nipun raj <103949203+nipunraj09@users.noreply.github.com>
2025-04-13 23:15:10 +0530

    Create newt.js

---
+++ newt.js
@@ -0,0 +1,9 @@
+import React from react;
+
+function MenuItem({image,name,price}){
+    return(<div className="menuItem">
+        <div style={{backgroundImage: `url($(image))` }}> </div>
+        <h1>  </h1>
+        <p> </p>
+        </div>);
+}
